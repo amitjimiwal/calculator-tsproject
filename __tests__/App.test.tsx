02357d95f9c147a5e0228e355475df3eb2bd553c
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-01 16:20:31 +0530

    base tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import {render} from '@testing-library/react'
-import App from '../App'
+import App from '../src/App'
 import expect from 'expect';
 describe(App,()=>{
          it("app display calculator text",()=>{
